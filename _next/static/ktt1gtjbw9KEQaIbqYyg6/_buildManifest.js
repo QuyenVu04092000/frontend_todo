b7f28b022e52574708624612a87b5c40e6de48f3
QuyenVu04092000 <QuyenVu04092000@users.noreply.github.com>
2025-11-11 09:36:48 +0000

    deploy: 9a87a75bc2e8053d6f800e4b7c0111a6cc5123f0

---
+++ _next/static/ktt1gtjbw9KEQaIbqYyg6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/887-3eba346bcd09645c.js","static/chunks/pages/index-b9a4f486f0853477.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
