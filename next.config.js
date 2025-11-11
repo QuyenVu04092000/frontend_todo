a4fe7986f8c18a3a3da935916023250922a7cfbe
quyenvu <quyenvu.uzumaki@gmail.com>
2025-11-11 16:35:38 +0700

    completed frontend

--- next.config.js
+++ next.config.js
@@ -1,22 +1,30 @@
-const withPWA = require("next-pwa")({
+const withPWA = require( "next-pwa" )( {
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development"
-});
+  disable: process.env.NODE_ENV === "development",
+} );
 
-module.exports = withPWA({
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export", // 👈 quan trọng cho GitHub Pages
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost"],
+    unoptimized: true, // 👈 tránh lỗi build khi export
+    domains: [ "localhost" ],
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "*",
-        pathname: "/uploads/**"
-      }
-    ]
-  }
-});
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+  basePath: isProd ? "/frontend_todo" : "", // 👈 repo name
+  assetPrefix: isProd ? "/frontend_todo/" : "",
+};
+
+module.exports = withPWA( nextConfig );
