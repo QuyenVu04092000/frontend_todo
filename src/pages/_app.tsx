98b16a96045eea15e083577ff55f8a132bbd5c4a
quyenvu <quyenvu.uzumaki@gmail.com>
2025-11-27 17:27:07 +0700

    fix: handle add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,11 +9,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#4f46e5" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/frontend_todo/manifest.json" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/frontend_todo/apple-touch-icon.png"
         />
       </Head>
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,18 +19,22 @@ class MyDocument extends Document {
           <link
             rel="icon"
             type="image/png"
-            href="/favicon-96x96.png"
+            href="/frontend_todo/favicon-96x96.png"
             sizes="96x96"
           />
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="/frontend_todo/favicon.svg"
+          />
+          <link rel="shortcut icon" href="/frontend_todo/favicon.ico" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/frontend_todo/apple-touch-icon.png"
           />
           <meta name="apple-mobile-web-app-title" content="MyWebSite" />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="/frontend_todo/site.webmanifest" />
         </Head>
         <body className="bg-gray-100 text-gray-900">
           <Main />
